9a01927b2655a78396d88cc967980e68673d95c1
dfourcfive <i.benadidou@esi-sba.dz>
2022-11-30 11:24:29 +0100

    add graph

--- car_sales_data.py
+++ car_sales_data.py
@@ -7,12 +7,20 @@ import numpy as np
 def get_autos_data():
     dcsv = pd.read_csv("data/autos.csv", encoding = 'ISO-8859-1')
     df_filtered = dcsv.set_index('brand').sort_values(by=['price'], ascending=True)
-    df_filtered = df_filtered[df_filtered['price'].str.replace(r'\D', '').astype(float) > 0]
+    #converting and sorting the price column
+    df_filtered = df_filtered[df_filtered['price'].str.replace('$', '',regex=True).replace(',', '',regex=True).astype(float) > 0]
+    df_filtered['price'] = df_filtered['price'].str.replace('$', '',regex=True).replace(',', '',regex=True)
+    df_filtered['price'] = pd.to_numeric(df_filtered['price'])
+    df_filtered= df_filtered.sort_values(by=['price'])
+    #converting the date time column
+    df_filtered['dateCrawled'] = pd.to_datetime(df_filtered['dateCrawled'])
     #remove unnecessary columns
-    df_filtered.drop(columns=['dateCrawled','seller','offerType'],axis=1,inplace=True)
+    #df_filtered.drop(columns=['dateCrawled','seller','offerType'],axis=1,inplace=True)
     return df_filtered
 
 df = get_autos_data()
+df.index.name='brand'
+
 st.title('Cars prices by brand')
 cars = st.selectbox(
     "Select a brand",list(np.unique(df.index)),0
@@ -20,3 +28,9 @@ cars = st.selectbox(
 
 data = df.loc[cars]
 st.write("cars data by brand ", data.sort_index())
+#data= data[data['dateCrawled'].str.split(' ')]
+dtframe = pd.DataFrame(data)
+df_melted = pd.melt(dtframe,id_vars=['price','dateCrawled'])
+
+
+st.line_chart(df_melted,x='dateCrawled',y='price')
